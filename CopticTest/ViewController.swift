b15cb7f66db05f2b87debec9aae1a79982241603
KevinMarco <kevinmarco9000@gmail.com>
2025-03-17 16:33:59 +0200

    Initial Commit

---
+++ CopticTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CopticTest
+//
+//  Created by kevin marco on 17/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
